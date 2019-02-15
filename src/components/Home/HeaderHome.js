2e1d827fdf3f254c4e316ea8b57a6b96bb114d27
Joey Ricketts <joeyricketts@Joeys-Air.hsd1.co.comcast.net>
2019-02-15 15:19:51 -0700

    styling home page

--- src/components/Home/HeaderHome.js
+++ src/components/Home/HeaderHome.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import styled from "@emotion/styled";
-
+import jsimage from "../../assets/jsimage.png";
 import Nav from "../NavBar/Nav";
 
 export default class HeaderHome extends Component {
@@ -8,22 +8,42 @@ export default class HeaderHome extends Component {
     return (
       <Container>
         <Nav />
-        <Title>Junior Projects Night</Title>
+        <SubContainer>
+          <Img src={jsimage} alt="background of javascript logo" />
+          <Title>Colorado JavaScript Project and Study Group</Title>
+        </SubContainer>
       </Container>
     );
   }
 }
 
 // CSS-in-JS styled components
-const Title = styled.h2`
-  color: #000;
-  display: flex;
-`;
 const Container = styled.div`
-  height: 100vh;
+  height: 600px;
   width: 100vw;
   background-color: #fff;
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+const SubContainer = styled.div`
+  height: 500px;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+`;
+const Title = styled.h1`
+  color: #fff;
+  padding-bottom: 100px;
+  padding-right: 150px;
+  width: 500px;
+  z-index: 1;
+`;
+// could probably use object-fit css property to do this background image better. also need a better res image.
+const Img = styled.img`
+  position: absolute;
+  height: 500px;
+  width: 100%;
 `;
